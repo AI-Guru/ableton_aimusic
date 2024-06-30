56b74ff26f4d6cc5f95c18b4d13ad144ad5ae85d
Tristan Behrens <tristan@ai-guru.de>
2024-06-30 20:23:16 +0200

    Migrates composer.

---
+++ projects/composer/composer.js
@@ -0,0 +1,16 @@
+// Start of file: /Users/tristanbehrens/Development/abletongpt/projects/sharedcode/header.js
+
+// Copyright 2024 Dr. Tristan Behrens.
+// All rights reserved.
+// This code is not for distribution. This code is not allowed to be modified.
+autowatch = 1;
+
+
+// End of file: /Users/tristanbehrens/Development/abletongpt/projects/sharedcode/header.js
+
+// Start of file: /Users/tristanbehrens/Development/abletongpt/projects/sharedcode/test.js
+
+// This is just a test file.
+
+// End of file: /Users/tristanbehrens/Development/abletongpt/projects/sharedcode/test.js
+
